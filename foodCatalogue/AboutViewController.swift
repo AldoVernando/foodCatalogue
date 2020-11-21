c0e113d1c9b3c8329941869411ad5e92cad11422
aldo.vernando <aldo.vernando@gmail.com>
2020-11-21 18:06:06 +0700

    Add about page

--- foodCatalogue/AboutViewController.swift
+++ foodCatalogue/AboutViewController.swift
@@ -9,9 +9,28 @@ import UIKit
 
 class AboutViewController: UIViewController {
     
+    @IBOutlet weak var profileImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        profileImage.layer.cornerRadius = profileImage.frame.width/2
+    }
+    
+    @IBAction func linkedInButtonClicked(_ sender: UIButton) {
+        if let url = URL(string: "https://linkedin.com/in/aldo-vernando") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    @IBAction func githubButtonClicked(_ sender: UIButton) {
+        if let url = URL(string: "https://github.com/AldoVernando") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    @IBAction func instagramButtonClicked(_ sender: UIButton) {
+        if let url = URL(string: "https://www.instagram.com/aldo.vernando") {
+            UIApplication.shared.open(url)
+        }
     }
 }
